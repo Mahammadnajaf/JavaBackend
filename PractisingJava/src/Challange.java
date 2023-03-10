c9291271412af1add55557d78f9e8020b7718105
mahammadnajafov <najafovmahammad@edu.bme.hu>
2023-03-10 21:28:04 +0100

    finishin methods

--- PractisingJava/src/Challange.java
+++ PractisingJava/src/Challange.java
@@ -1,17 +1,21 @@
 public class Challange {
     public static void main(String[] args) {
-        calculatescore(true,800,5,100);
-        calculatescore(true,10000,8,200);
+
+        int highschore=calculatescore(false,2000,4,500);
+        System.out.println("highest score "+ highschore  );
+
+
+        System.out.println( "another hiigh score "+calculatescore(true,10000,8,200));
     }
 
-    public static void calculatescore(boolean gameOver,int score,int levelCompleted,int bonus) {
+    public static int calculatescore(boolean gameOver,int score,int levelCompleted,int bonus) {
         int finalScore=score;
 
         if(gameOver){
             finalScore+=(levelCompleted*bonus);
             finalScore+=1000;
-            System.out.println("bro ur final score is "+finalScore);
-        }
 
+        }
+        return finalScore;
     }
 }
